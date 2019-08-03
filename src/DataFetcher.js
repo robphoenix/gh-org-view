ec7619404af598ffdfb0e03236aaa49fbe0fa9c3
Rob Phoenix <rob@robphoenix.com>
2019-08-03 10:23:27 +0100

    Pairing with Owain

---
+++ src/DataFetcher.js
@@ -0,0 +1,46 @@
+// @ts-nocheck
+import React from 'react'
+import { gql } from 'apollo-boost'
+
+const DataFetcher = ({ client }) => {
+  const [repos, setRepos] = React.useState([])
+
+  const query = gql`
+    query {
+      organization(login: "uw-labs") {
+        repositories(first: 50) {
+          nodes {
+            id
+            url
+            name
+          }
+        }
+      }
+    }
+  `
+
+  React.useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const { data } = await client.query({ query })
+        setRepos(data.organization.repositories.nodes)
+        console.log({ data })
+      } catch (error) {
+        console.log({ error })
+      }
+    }
+    fetchData()
+  }, [client, query])
+
+  return (
+    <div>
+      <ul>
+        {repos.map(repo => (
+          <li key={repo.id}>{repo.name}</li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default DataFetcher
